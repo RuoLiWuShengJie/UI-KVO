ad762312d3e0703aad540dbe6fc4183ac95c9fc4
吴胜杰 <469912605@qq.com@wushengjie.local>
2016-01-20 14:57:16 +0800

    Initial Commit

---
+++ UI-KVO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UI-KVO
+//
+//  Created by 吴胜杰 on 16/1/20.
+//  Copyright © 2016年 wushengjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UI-KVO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UI-KVO
+//
+//  Created by 吴胜杰 on 16/1/20.
+//  Copyright © 2016年 wushengjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
